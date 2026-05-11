fc61edc36f6360f86065fb4b452271feb676ea82
Gusev Vladimir <162140064+Gusev-V@users.noreply.github.com>
2026-05-11 22:03:01 +0300

    hw-3
    - Добавлены модели: Profile (OneToOne к User), Question, Answer, Tag,
    - Реализован кастомный QuestionManager с методами выборки:
    - Добавлено кеширование счетчиков лайков (likes_count) с автообновлением
    - Настроена админка на русском языке с оптимизацией запросов
    - Создана management-команда fill_db для наполнения БД тестовыми данными
    - Обновлены контекст-процессоры для работы с реальными данными из БД
    - Настроена MySQL база данных через .env файл
    - Добавлены состояния пустоты (empty states) для всех страниц

--- ASK_ME/context_processors.py
+++ ASK_ME/context_processors.py
@@ -1,28 +1,41 @@
+from django.db.models import Count
+from questions.models import Tag
+from django.contrib.auth.models import User
+
+
 def sidebar(request):
-    popular_tags = ['perl', 'python', 'MySQL', 'django', 'Mail.ru', 'Voloshin', 'Firefox']
-    best_members = ['Mr.Freeman', 'Dr.House', 'Bender', 'Queen Victoria', 'V.Pupkin']
+    popular_tags = Tag.objects.annotate(
+        questions_cnt=Count('questions')
+    ).order_by('-questions_cnt')[:10]
+
+    best_members = User.objects.annotate(
+        total=Count('questions', distinct=True) + Count('answers', distinct=True)
+    ).filter(
+        total__gt=0
+    ).select_related('profile').order_by('-total')[:10]
+
     return {
         'popular_tags': popular_tags,
         'best_members': best_members,
     }
 
 
-def user(request):
-    username = 'Dr. Pepper'
-    user_icon = 'img/user-icon.svg'
-    user_email = 'dr.pepper-+@@@mail.ru'
-    login = 'dr_pepper'
-
-    excluded_urls = ['login', 'registration']
-
-    if request.resolver_match and request.resolver_match.url_name in excluded_urls:
-        return {}
+def current_user_info(request):
+    # username = 'Dr. Pepper'
+    # user_icon = 'img/user-icon.svg'
+    # user_email = 'dr.pepper-+@@@mail.ru'
+    # login = 'dr_pepper'
+    #
+    # excluded_urls = ['login', 'registration']
+    #
+    # if request.resolver_match and request.resolver_match.url_name in excluded_urls:
+    #     return {}
 
     return {
-        'user': {
-            'username': username,
-            'icon': user_icon,
-            'login': login,
-            'email': user_email,
-        }
+        # 'current_user_info': {
+        #     'username': username,
+        #     'icon': user_icon,
+        #     'login': login,
+        #     'email': user_email,
+        # }
     }
